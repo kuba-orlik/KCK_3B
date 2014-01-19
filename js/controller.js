71657c9cf6362dc328483bd231b683b1eeb32d7c
Kuba Orlik <groovy354@gmail.com>
2014-01-19 09:36:55 +0100

    polskie znaki w czcionce

--- js/controller.js
+++ js/controller.js
@@ -148,6 +148,9 @@ scheme_collection = new function(){
 	            controller.main_hero.translate(coords.x, coords.y);        		
         	}
         }),
+        new scheme("(elo|witaj|siema|joł|cześć|czesc)", function(){
+        	say('Dzień dobry. Mamy dzisiaj piękny dzień');
+        }),
         new scheme("(opowiedz mi (z|ż)art|tell (me)? (a)? joke|rozbaw mnie|jest mi smutno|smutno mi|walnij suchara|corny joke|Krzysiu Weiss)", function(){
         	$.get('http://api.icndb.com/jokes/random', function(data){
         		say("Ok, znasz ten angielski żart? <i>" + data.value.joke + "</i>");
